d8ca5aac3e6a254a46e2d3a4ae16033050aaba91
haya4 <hayashi.yuu@gmail.com>
2022-05-06 22:32:51 +0900

    update tile.

--- index.js
+++ index.js
@@ -78,41 +78,11 @@ var mvtSource0 = new L.TileLayer.MVTSource({
 });
 map.addLayer(mvtSource0);
 
-var mvtSource1 = new L.TileLayer.MVTSource({
-    url: "https://yuuhayashi.github.io/coverageWeb/tiles/tileBus1/{z}/{x}/{y}.pbf",
-    style: function (feature) {
-        var style = {};
-        style.color = 'rgba(255,200,0,1)';
-        style.radius = 5;
-        style.selected = {
-          radius: 6
-        };
-        return style;
-    }
-});
-map.addLayer(mvtSource1);
-
-var mvtSource2 = new L.TileLayer.MVTSource({
-    url: "https://yuuhayashi.github.io/coverageWeb/tiles/tileBus2/{z}/{x}/{y}.pbf",
-    style: function (feature) {
-        var style = {};
-        style.color = 'rgba(0,255,0,1)';
-        style.radius = 5;
-        style.selected = {
-          radius: 6
-        };
-        return style;
-    }
-});
-map.addLayer(mvtSource2);
-
 //Globals that we can change later.
 var fillColor = 'rgba(149,139,255,0.4)';
 var strokeColor = 'rgb(20,20,20)';
 
 //Add layer
-mvtSource2.addTo(map);
-mvtSource1.addTo(map);
 mvtSource0.addTo(map);
 
 L.control.scale({imperial:false}).addTo(map);
